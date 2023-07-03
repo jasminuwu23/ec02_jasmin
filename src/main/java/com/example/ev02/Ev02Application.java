a11acd5dbe04b0ad7b4a6ad92d1af29920ce5028
jasminuwu23 <124199684+jasminuwu23@users.noreply.github.com>
2023-07-03 12:03:02 -0500

    Add files via upload

---
+++ src/main/java/com/example/ev02/Ev02Application.java
@@ -0,0 +1,13 @@
+package com.example.ev02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ev02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ev02Application.class, args);
+	}
+
+}
